ac287c7366f4770e0c1c8ff468491cf46cb86b05
m0nclous <m0nclous@vk.com>
2022-10-22 01:35:54 +0600

    Добавлен флаг usePolling в конфиг vite для watch (чтоб работало на WSL 2)

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        watch: {
+            usePolling: true
+        }
+    },
+
     plugins: [
         vue()
     ]
