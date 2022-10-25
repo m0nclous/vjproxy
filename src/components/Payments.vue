37c8b8f976dde57cbfcb991ec929bd3cd3ff1992
m0nclous <m0nclous@vk.com>
2022-10-25 22:10:46 +0600

    Реализация блока payments

---
+++ src/components/Payments.vue
@@ -0,0 +1,86 @@
+<template>
+    <div class="payments">
+        <div class="container">
+            <h2>Удобные способы оплаты</h2>
+
+            <div class="payments-list">
+                <div v-for="payment in payments" class="payments-item">
+                    <img :src="payment.img.src" :alt="payment.img.alt">
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    import visa from '../assets/payments/visa.png';
+    import mastercard from '../assets/payments/mastercard.png';
+    import mir from '../assets/payments/mir.png';
+    import yandex from '../assets/payments/yandex.png';
+    import qiwi from '../assets/payments/qiwi.png';
+    import bitcoin from '../assets/payments/bitcoin.png';
+    import webmoney from '../assets/payments/webmoney.png';
+
+    const payments = [
+        {
+            img: {
+                src: visa,
+                alt: 'Visa'
+            }
+        },
+        {
+            img: {
+                src: mastercard,
+                alt: 'Mastercard'
+            }
+        },
+        {
+            img: {
+                src: mir,
+                alt: 'MIR'
+            }
+        },
+        {
+            img: {
+                src: yandex,
+                alt: 'Yandex Money'
+            }
+        },
+        {
+            img: {
+                src: qiwi,
+                alt: 'QIWI'
+            }
+        },
+        {
+            img: {
+                src: bitcoin,
+                alt: 'Bitcoin'
+            }
+        },
+        {
+            img: {
+                src: webmoney,
+                alt: 'Webmoney'
+            }
+        }
+    ];
+</script>
+
+<style lang="scss" scoped>
+    .payments {
+        width: 100%;
+        background: #FFFFFFD1;
+        padding: 100px 0;
+
+        &-list {
+            display: flex;
+        }
+    }
+
+    h2 {
+        font-size: 2rem;
+        color: var(--color-black);
+        text-align: center;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
     <section class="page-content">
         <Packs />
         <Advantages />
+        <Payments />
     </section>
 </template>
 
 <script lang="ts" setup>
     import Packs from '../components/Packs.vue';
     import Advantages from '../components/Advantages.vue';
+    import Payments from '../components/Payments.vue';
 </script>
 
 <style lang="scss" scoped>
