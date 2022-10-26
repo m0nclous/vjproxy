f78d9f092843c3887048faa0014a26d6726f5bc5
m0nclous <m0nclous@vk.com>
2022-10-27 00:33:28 +0600

    Добавлен и настроен модуль vite-plugin-html

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
     server: {
@@ -9,6 +10,26 @@ export default defineConfig({
     },
 
     plugins: [
-        vue()
+        vue(),
+
+        createHtmlPlugin({
+            entry: 'src/main.ts',
+
+            inject: {
+                data: {
+                    title: 'VJProxy'
+                },
+
+                tags: [
+                    {
+                        injectTo: 'body-prepend',
+                        tag: 'div',
+                        attrs: {
+                            id: 'app'
+                        }
+                    }
+                ]
+            }
+        })
     ]
 });
